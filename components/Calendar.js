c8441d96d13e9b653b85df31f63da68a4ab32349
Rissa Kei Chua <78063215+rissakei@users.noreply.github.com>
2023-12-02 14:25:58 -0500

    Update Calendar.js

--- components/Calendar.js
+++ components/Calendar.js
@@ -1,64 +1,145 @@
-import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import React, { useState } from 'react';
+import { View, Text, TouchableOpacity, StyleSheet } from 'react-native';
+import moment from 'moment'; // Ensure moment is installed
 
-const CalendarComponent = () => {
-    const styles = StyleSheet.create({
-        calendarContainer: {
-            padding: 10,
-            backgroundColor: '#FF5A5F', // Header background color
-            borderRadius: 10,
-            // Other shadow properties can remain the same
-        },
-        header: {
-            fontSize: 24,
-            fontWeight: 'bold',
-            textAlign: 'center',
-            padding: 10,
-            color: '#fff', // Text color for the header
-        },
-        week: {
-            flexDirection: 'row',
-            justifyContent: 'space-around',
-        },
-        day: {
-            width: 40,
-            height: 40,
-            justifyContent: 'center',
-            alignItems: 'center',
-            margin: 5,
-            // The day background is transparent for non-selected days
-        },
-        dayText: {
-            fontSize: 16,
-            fontWeight: 'bold',
-            color: 'grey', // Text color for non-selected days
-        },
-        currentDay: {
-            backgroundColor: '#FC6E77', // Background color for the selected day
-            borderRadius: 20, // Circular background for the current day
-        },
-        currentDayText: {
-            color: '#fff', // Text color for the selected day
-        },
-    });
+// styles
+const styles = StyleSheet.create({
+  calendarContainer: {
+    backgroundColor: '#FFFFFF',
+    borderRadius: 10,
+    margin: 10,
+    shadowColor: '#C0C0C0',
+    shadowOffset: { width: 0, height: 0 },
+    shadowOpacity: 0.15,
+    shadowRadius: 15,
+    fontSize: 13,
+    textAlign: 'center',
+  },
+  header: {
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#2875C7',
+    color: '#FFFFFF',
+    padding: 10,
+  },
+  monthLabel: {
+    fontSize: 14,
+    fontWeight: 'bold',
+    color: '#FFFFFF',
+    textTransform: 'uppercase',
+  },
+  arrow: {
+    padding: 10,
+    fontSize: 14,
+    fontWeight: 'bold',
+    color: '#FFFFFF',
+  },
+  week: {
+    flexDirection: 'row',
+    borderBottomWidth: 1,
+    borderBottomColor: '#CCCCCC',
+  },
+  day: {
+    flex: 1,
+    padding: 10,
+    borderWidth: 1,
+    borderColor: '#CCCCCC',
+  },
+  dayText: {
+    textAlign: 'center',
+    fontWeight: 'bold',
+  },
+});
 
-    const renderDay = (day, isSelected) => (
-        <View style={[styles.day, isSelected && styles.currentDay]} key={day.toString()}>
-            <Text style={[styles.dayText, isSelected && styles.currentDayText]}>
-                {day}
-            </Text>
-        </View>
-    );
-
-    return (
-        <View style={styles.calendarContainer}>
-            <Text style={styles.header}>November 2023</Text>
-            <View style={styles.week}>
-                {['S', 'M', 'T', 'W', 'T', 'F', 'S'].map((day) => renderDay(day, day === '15'))}
-            </View>
-            {/* Additional weeks would be rendered similarly */}
-        </View>
-    );
+// DayNames Component
+const DayNames = () => {
+  return (
+    <View style={styles.week}>
+      {['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'].map((dayName) => (
+        <Text style={styles.day} key={dayName}>{dayName}</Text>
+      ))}
+    </View>
+  );
 };
 
-export default CalendarComponent;
+// Day Component
+const Day = ({ day, select, selected }) => {
+  const dayStyle = [
+    styles.day,
+    day.isToday ? { backgroundColor: '#D3EFFF' } : null,
+    day.isCurrentMonth ? null : { color: '#C0C0C0' },
+    day.date.isSame(selected) ? { backgroundColor: '#2875C7', color: '#FFFFFF' } : null,
+  ];
+
+  return (
+    <TouchableOpacity style={dayStyle} onPress={() => select(day)}>
+      <Text style={styles.dayText}>{day.number}</Text>
+    </TouchableOpacity>
+  );
+};
+
+// Week Component
+const Week = ({ date, month, select, selected }) => {
+  let days = [];
+  for (let i = 0; i < 7; i++) {
+    const day = {
+      name: date.format("dd").substring(0, 1),
+      number: date.date(),
+      isCurrentMonth: date.month() === month.month(),
+      isToday: date.isSame(new Date(), "day"),
+      date: date.clone(),
+    };
+    days.push(<Day key={day.date.toString()} day={day} select={select} selected={selected} />);
+    date.add(1, "day");
+  }
+
+  return <View style={styles.week}>{days}</View>;
+};
+
+// Calendar Component
+const Calendar = () => {
+  const [month, setMonth] = useState(moment());
+  const [selected, setSelected] = useState(moment().startOf('day'));
+
+  const previous = () => {
+    setMonth(prevMonth => prevMonth.clone().subtract(1, 'month'));
+  };
+
+  const next = () => {
+    setMonth(prevMonth => prevMonth.clone().add(1, 'month'));
+  };
+
+  const select = (day) => {
+    setSelected(day.date);
+    setMonth(day.date.clone());
+  };
+
+  const renderWeeks = () => {
+    let weeks = [];
+    let done = false;
+    let date = month.clone().startOf("month").day("Sunday");
+    let monthIndex = date.month();
+
+    while (!done) {
+      weeks.push(
+        <Week key={date.toString()} date={date.clone()} month={month} select={select} selected={selected} />
+      );
+      date.add(1, "week");
+      done = monthIndex !== date.month();
+      monthIndex = date.month();
+    }
+
+    return weeks;
+  };
+
+  const renderMonthLabel = () => {
+    return <Text style={styles.monthLabel}>{month.format("MMMM YYYY")}</Text>;
+  };
+
+  return (
+    <View style={styles.calendarContainer}>
+      <View style={styles.header}>
+        <TouchableOpacity onPress={previous}>
+          <Text style={styles.arrow
