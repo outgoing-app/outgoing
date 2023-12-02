aeced4881a150019b544035478f28ac58a3e1249
Rissa Kei Chua <78063215+rissakei@users.noreply.github.com>
2023-12-02 18:03:57 -0500

    Update HomeScreen.js

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,43 +1,73 @@
 import React from 'react';
-import { View, StyleSheet, ImageBackground } from 'react-native';
-import image from '../assets/screen-background.png';
-import { useFonts, PublicSans_700Bold, PublicSans_400Regular } from "@expo-google-fonts/public-sans";
-import CalendarComponent from '../components/CalendarComponent'; 
+import { View, StyleSheet, ImageBackground, ScrollView, Text } from 'react-native';
+import Header from '../components/Header'; 
+import NavBar from '../components/NavBar'; 
 
 const HomeScreen = () => {
-    const [fontsLoaded] = useFonts({
-        PublicSans_700Bold,
-        PublicSans_400Regular,
-    });
-
     const styles = StyleSheet.create({
         container: {
             flex: 1,
+            backgroundColor: '#FFFFFF',
         },
-        image: {
-            flex: 1,
-            resizeMode: "cover",
-            justifyContent: "center",
+        scrollViewContent: {
+            paddingHorizontal: 10,
         },
-        header: {
-            fontSize: 24,
-            fontWeight: 'bold',
+        tabContainer: {
+            flexDirection: 'row',
+            justifyContent: 'space-around',
+            paddingVertical: 10,
+        },
+        tab: {
+            backgroundColor: '#FF7880', 
+            borderRadius: 20,
+            padding: 10,
+            paddingHorizontal: 20,
+        },
+        tabText: {
+            color: '#FFFFFF',
+            fontSize: 16,
+        },
+        eventPlaceholder: {
+            backgroundColor: '#FF7880', 
+            borderRadius: 12,
+            padding: 20,
+            marginHorizontal: 20,
+            marginVertical: 10,
+        },
+        eventText: {
+            color: '#FFFFFF',
+            fontSize: 16,
+        },
+        dateText: {
             textAlign: 'center',
-            margin: 10,
-        },
-        mainContent: {
-            flex: 1,
-            justifyContent: 'center', 
-            alignItems: 'center', 
+            color: '#FF7880', 
+            fontSize: 16,
+            marginVertical: 10,
         },
     });
 
     return (
         <View style={styles.container}>
-            <ImageBackground source={image} style={styles.image}>
-                <View style={styles.mainContent}>
-                    <CalendarComponent />
-                </View>
+            <ImageBackground source={require('../assets/background.png')} style={{ flex: 1 }}>
+                <Header /> 
+                <ScrollView contentContainerStyle={styles.scrollViewContent}>
+                    <View style={styles.tabContainer}>
+                        <View style={styles.tab}><Text style={styles.tabText}>Monthly</Text></View>
+                        <View style={styles.tab}><Text style={styles.tabText}>Weekly</Text></View>
+                        <View style={styles.tab}><Text style={styles.tabText}>Daily</Text></View>
+                    </View>
+                    {/* placeholder for event date */}
+                    <Text style={styles.dateText}>November 2023</Text>
+                    {/* placeholder for event */}
+                    <View style={styles.eventPlaceholder}>
+                        <Text style={styles.eventText}>Lunch at Fumo 12PM - 2PM</Text>
+                    </View>
+                    {/* placeholder for event */}
+                    <View style={styles.eventPlaceholder}>
+                        <Text style={styles.eventText}>Ktown Karaoke 11PM - 1AM</Text>
+                    </View>
+                </ScrollView>
+                <NavBar />
             </ImageBackground>
         </View>
     );
