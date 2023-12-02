c751b437f086ee032f47da1db691e7812ef1981c
Rissa Kei Chua <78063215+rissakei@users.noreply.github.com>
2023-12-02 11:49:36 -0500

    updated Calendar component

--- components/Calendar.js
+++ components/Calendar.js
@@ -5,22 +5,16 @@ const CalendarComponent = () => {
     const styles = StyleSheet.create({
         calendarContainer: {
             padding: 10,
-            backgroundColor: '#fff',
+            backgroundColor: '#FF5A5F', // Header background color
             borderRadius: 10,
-            shadowColor: "#000",
-            shadowOffset: {
-                width: 0,
-                height: 2,
-            },
-            shadowOpacity: 0.25,
-            shadowRadius: 3.84,
-            elevation: 5,
+            // Other shadow properties can remain the same
         },
         header: {
             fontSize: 24,
             fontWeight: 'bold',
             textAlign: 'center',
             padding: 10,
+            color: '#fff', // Text color for the header
         },
         week: {
             flexDirection: 'row',
@@ -32,18 +26,27 @@ const CalendarComponent = () => {
             justifyContent: 'center',
             alignItems: 'center',
             margin: 5,
-            borderRadius: 20,
-            backgroundColor: 'lightgrey',
+            // The day background is transparent for non-selected days
         },
         dayText: {
             fontSize: 16,
             fontWeight: 'bold',
+            color: 'grey', // Text color for non-selected days
+        },
+        currentDay: {
+            backgroundColor: '#FC6E77', // Background color for the selected day
+            borderRadius: 20, // Circular background for the current day
+        },
+        currentDayText: {
+            color: '#fff', // Text color for the selected day
         },
     });
 
-    const renderDay = (day) => (
-        <View style={styles.day} key={day}>
-            <Text style={styles.dayText}>{day}</Text>
+    const renderDay = (day, isSelected) => (
+        <View style={[styles.day, isSelected && styles.currentDay]} key={day.toString()}>
+            <Text style={[styles.dayText, isSelected && styles.currentDayText]}>
+                {day}
+            </Text>
         </View>
     );
 
@@ -51,7 +54,7 @@ const CalendarComponent = () => {
         <View style={styles.calendarContainer}>
             <Text style={styles.header}>November 2023</Text>
             <View style={styles.week}>
-                {['S', 'M', 'T', 'W', 'T', 'F', 'S'].map(renderDay)}
+                {['S', 'M', 'T', 'W', 'T', 'F', 'S'].map((day) => renderDay(day, day === '15'))}
             </View>
             {/* Additional weeks would be rendered similarly */}
         </View>
