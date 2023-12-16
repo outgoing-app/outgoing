07047318e0a1bc597311aaefc3afa1addf8ef98f
Tatt Chitrakorn <tatt.chitrakorn@gmail.com>
2023-12-15 20:22:34 -0500

    Updated VollingPoll.js

--- screens/VotingPoll.js
+++ screens/VotingPoll.js
@@ -1,5 +1,5 @@
-import React, { useEffect , useState} from 'react';
-import { View, Text, StyleSheet, ImageBackground, Button, Modal} from 'react-native';
+import React, { useEffect, useState } from 'react';
+import { View, Text, StyleSheet, ImageBackground, Button, Modal } from 'react-native';
 import { NavigationContainer, useNavigation } from '@react-navigation/native';
 import { PublicSans_700Bold, PublicSans_400Regular, useFonts } from "@expo-google-fonts/public-sans";
 import BackButton from '../components/BackButton';
@@ -15,25 +15,25 @@ const waitingInitials = ['AS', 'BO', 'PD', 'LS'];
 const confirmedInitials = ['MS', 'KZ'];
 // may need mapping function based on structure of db
 const times = [
-    {index: 0, time: '11/27/23 @ 6:00 PM - 8:00 PM'}, 
-    {index: 1, time: '11/28/23 @ 9:00 PM - 11:00 PM'}, 
-    {index: 2, time: '11/29/23 @ 10:00 AM - 12:00 PM'}
+    { index: 0, time: '11/27/23 @ 6:00 PM - 8:00 PM' },
+    { index: 1, time: '11/28/23 @ 9:00 PM - 11:00 PM' },
+    { index: 2, time: '11/29/23 @ 10:00 AM - 12:00 PM' }
 ];
 
 
-const VotingPoll = ({isVisible, onClose}) => {
+const VotingPoll = ({ isVisible, onClose }) => {
     const navigation = useNavigation();
     const [fontsLoaded] = useFonts({
         PublicSans_700Bold,
         PublicSans_400Regular,
     });
     // keeps track of checked options
-    const[checkedState, setCheckedState] = useState(
+    const [checkedState, setCheckedState] = useState(
         new Array(times.length).fill(false)
     );
     // disable vote button after voting
-    const[disabled, setDisabled] = useState(false);
-    const[voted, setVoted] = useState('Vote')
+    const [disabled, setDisabled] = useState(false);
+    const [voted, setVoted] = useState('Vote')
     // updates checked options
     const handleChange = (idx) => {
         const updatedCheckedState = checkedState.map((item, index) =>
@@ -48,7 +48,7 @@ const VotingPoll = ({isVisible, onClose}) => {
         setDisabled(true)
         setVoted('Voted!')
         // update database with # votes for each option
-        
+
     }
 
     const styles = StyleSheet.create({
@@ -60,40 +60,40 @@ const VotingPoll = ({isVisible, onClose}) => {
             justifyContent: 'center',
         },
         outerContainer: {
-          flexDirection: 'column',
-          width: 350,
-          alignSelf: 'center',
-          padding: 20,
-          backgroundColor: 'white',
-          borderRadius: 8,
-          borderColor: '#FC6E77',
-          borderWidth: 2,
-          marginBottom: 0,
-          marginTop: 150,
+            flexDirection: 'column',
+            width: 350,
+            alignSelf: 'center',
+            padding: 20,
+            backgroundColor: 'white',
+            borderRadius: 8,
+            borderColor: '#FC6E77',
+            borderWidth: 2,
+            marginBottom: 0,
+            marginTop: 110,
         },
         innerContainer: {
-          marginVertical: 5,
-          marginLeft: 5,
-          flexDirection: 'column',
+            marginVertical: 5,
+            marginLeft: 5,
+            flexDirection: 'column',
         },
         headerText: {
-          fontSize: 22,
-          color: '#FC6E77',
-          textAlign: 'center',
-          fontWeight: 'bold',
+            fontSize: 22,
+            color: '#FC6E77',
+            textAlign: 'center',
+            fontWeight: 'bold',
         },
-        subText:{
-          fontSize: 16,
-          textAlign: 'left',
-          color: '#FC6E77',
-          fontWeight: 'bold',
-          marginBottom: 5,
+        subText: {
+            fontSize: 16,
+            textAlign: 'left',
+            color: '#FC6E77',
+            fontWeight: 'bold',
+            marginBottom: 5,
         },
-        detailsContainer:{
-          flexDirection: 'row',
-          gap: 40,
+        detailsContainer: {
+            flexDirection: 'row',
+            gap: 40,
         },
-        pollContainer:{
+        pollContainer: {
             flexDirection: 'column',
             width: 273,
             alignSelf: 'center',
@@ -102,62 +102,62 @@ const VotingPoll = ({isVisible, onClose}) => {
             borderColor: '#FF7880',
             borderWidth: 1,
         },
-        detailsLabel:{
-          color: '#8B8B8B',
-          width: 50,
-          marginVertical: 7,
+        detailsLabel: {
+            color: '#8B8B8B',
+            width: 50,
+            marginVertical: 7,
         },
-        detailsText:{
-          color:'black',
-          marginVertical: 5,
+        detailsText: {
+            color: 'black',
+            marginVertical: 5,
         },
     });
 
     return (
         <Modal transparent={true} visible={isVisible}>
-        <View style={styles.outerContainer}>
-            <Text style={styles.headerText}>{eventName}</Text>
-            <View
-                style={{
-                borderBottomColor: '#FC6E77',
-                borderBottomWidth: 2,
-                width: 350,
-                alignSelf: 'center',
-                marginTop: 15,
-                marginBottom: 10,
-                }}
-            />
-            <View style={styles.innerContainer}>
-                <Text style={styles.subText}>Waiting on</Text>
-                <View style={{flexDirection: 'row'}}>
-                    {waitingInitials.map((name) => <UserIcon initials={name} size={35} />)}
+            <View style={styles.outerContainer}>
+                <Text style={styles.headerText}>{eventName}</Text>
+                <View
+                    style={{
+                        borderBottomColor: '#FC6E77',
+                        borderBottomWidth: 2,
+                        width: 350,
+                        alignSelf: 'center',
+                        marginTop: 15,
+                        marginBottom: 10,
+                    }}
+                />
+                <View style={styles.innerContainer}>
+                    <Text style={styles.subText}>Waiting on</Text>
+                    <View style={{ flexDirection: 'row' }}>
+                        {waitingInitials.map((name) => <UserIcon initials={name} size={35} />)}
+                    </View>
+                </View>
+                <View style={styles.innerContainer}>
+                    <Text style={styles.subText}>Confirmed</Text>
+                    <View style={{ flexDirection: 'row' }}>
+                        {confirmedInitials.map((name) => <UserIcon initials={name} size={35} />)}
+                    </View>
+                </View>
+                <View style={styles.pollContainer}>
+                    <Text style={styles.subText}>Vote on time(s):</Text>
+                    {times.map((time) =>
+                        <CheckBox
+                            title={time.time}
+                            size={20}
+                            margin={0}
+                            checked={checkedState[time.index]}
+                            onPress={() => {
+                                handleChange(time.index);
+                            }}
+                            textStyle={{ fontWeight: 'normal', fontSize: 12, color: '8B8B8B' }}
+                        />)}
+                </View>
+                <View style={{ alignItems: 'center', flexDirection: 'row' }}>
+                    <CreateButtonSmall buttonText="Close" onPress={onClose} disabled={false} />
+                    <CreateButtonSmall buttonText={voted} onPress={vote} disabled={disabled} />
                 </View>
             </View>
-            <View style={styles.innerContainer}>
-                <Text style={styles.subText}>Confirmed</Text>
-                <View style={{flexDirection: 'row'}}>
-                    {confirmedInitials.map((name) => <UserIcon initials={name} size={35} />)}
-                </View>
-            </View>
-            <View style={styles.pollContainer}>
-                <Text style={styles.subText}>Vote on time(s):</Text>
-                {times.map((time) => 
-                <CheckBox 
-                title={time.time} 
-                size={20}
-                margin={0}
-                checked = {checkedState[time.index]}
-                onPress={()=>{
-                    handleChange(time.index);
-                }}
-                textStyle={{fontWeight:'normal', fontSize: 12, color:'8B8B8B'}}
-                />)}
-            </View>
-            <View style={{alignItems: 'center', flexDirection: 'row'}}>
-                <CreateButtonSmall buttonText="Close" onPress={onClose} disabled={false}/>
-                <CreateButtonSmall buttonText={voted} onPress={vote} disabled={disabled}/>
-            </View>
-        </View>
         </Modal>
     );
 };
